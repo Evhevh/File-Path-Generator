aacef0b8c07a07c41b32d15f63d5f7f3c3d28df6
Evhevh <32005445+Evhevh@users.noreply.github.com>
2025-05-17 19:01:14 -0700

    Update defaultPaths.js

--- number-image-microservice/src/config/defaultPaths.js
+++ number-image-microservice/src/config/defaultPaths.js
@@ -1,4 +1,4 @@
-const DEFAULT_IMAGE_PATH = 'C:/Users/Evhev/Desktop/School/Spring_2025/CS_361/Assignment 1/cs361/';
+const DEFAULT_IMAGE_PATH = 'insert folder with images here';
 
 module.exports = {
     DEFAULT_IMAGE_PATH
